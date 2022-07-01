e34cd4559a566301aa65cad6cfeeac1fb3a9ab35
chamchipack <90598408+chamchipack@users.noreply.github.com>
2022-07-01 12:03:25 +0900

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {userData,alldataserch,onedataserch,daydataserch,dataDelete,dataModify,namedataserch,userMemo} = require('../controllers/post')
+const {userData,alldatasearch,onedatasearch,daydatasearch,dataDelete,dataModify,namedatasearch,userMemo} = require('../controllers/post')
 const authMiddleware = require('../middleware/authMiddleware')
 require("dotenv").config()
 
@@ -8,16 +8,16 @@ require("dotenv").config()
 router.post('/userData', authMiddleware, userData)
 
 //유저 데이터 가져오기(전체) API
-router.get('/alldataserch', authMiddleware, alldataserch)
+router.get('/alldataserch', authMiddleware, alldatasearch)
 
 //유저 데이터 가져오기(한개) API
-router.get('/onedataserch', authMiddleware, onedataserch)
+router.get('/onedataserch', authMiddleware, onedatasearch)
 
 //유저 이름검색 가져오기(한개) API
-router.get('/namedataserch', authMiddleware, namedataserch)
+router.get('/namedataserch', authMiddleware, namedatasearch)
 
 //유저 데이터 가져오기(요일만) API
-router.get('/daydataserch', authMiddleware, daydataserch)
+router.get('/daydataserch', authMiddleware, daydatasearch)
 
 //유저 데이터 삭제하기 API
 router.delete('/dataDelete', authMiddleware, dataDelete)
